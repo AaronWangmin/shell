27b11dc45b5045d7dc7998347c47796b21cad168
wang min <minwang80@163.com>
2020-10-07 13:30:28 +0800

    Update cp_file2file.sh

--- cp_file2file.sh
+++ cp_file2file.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #echo "Hello World !"
+
 targetArray=($(ls /home/slam/75_6/AD50 -r))
 sourceArray=($(ls /home/slam/50/50 -r))
-targetNumber=1
-i=1
-while(($i<=53))
+
+i=0
+while(($i<53))
 do
-    echo $i:  ${targetArray[$i-1]}
-    echo "slam" | sudo cp /home/slam/50/50/${sourceArray[$i-1]}/*.ASC /home/slam/75_6/AD50/${targetArray[$i-1]}
+    echo $i:  ${targetArray[$i]}
+    echo "slam" | sudo cp /home/slam/50/50/${sourceArray[$i]}/*.ASC /home/slam/75_6/AD50/${targetArray[$i]}
     let ++i
 done
 
-k=1
-while(($k<=53))
+k=0
+while(($k<53))
 do
-    echo $k:  ${sourceArray[$k-1]}
+    echo $k:  ${sourceArray[$k]}
     let k++
 done
 
